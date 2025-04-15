4b1825cea41406db408b7d1574a2f0f87416bf30
Prana Mohanty <94722016+PranaMohanty13@users.noreply.github.com>
2025-04-14 20:25:01 -0500

    canging api key name

---
+++ insight-explorer/components/LoadingAnimation.tsx
@@ -0,0 +1,178 @@
+import React from "react";
+import styles from "./LoadingAnimation.module.scss";
+
+const LoadingAnimation = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.h1Container}>
+        <div className={`${styles.cube} ${styles.h1} ${styles.w1} ${styles.l1}`}>
+          <div className={`${styles.face} ${styles.top}`}></div>
+          <div className={`${styles.face} ${styles.left}`}></div>
+          <div className={`${styles.face} ${styles.right}`}></div>
+        </div>
+
+        <div className={`${styles.cube} ${styles.h1} ${styles.w1} ${styles.l2}`}>
+          <div className={`${styles.face} ${styles.top}`}></div>
+          <div className={`${styles.face} ${styles.left}`}></div>
+          <div className={`${styles.face} ${styles.right}`}></div>
+        </div>
+
+        <div className={`${styles.cube} ${styles.h1} ${styles.w1} ${styles.l3}`}>
+          <div className={`${styles.face} ${styles.top}`}></div>
+          <div className={`${styles.face} ${styles.left}`}></div>
+          <div className={`${styles.face} ${styles.right}`}></div>
+        </div>
+
+        <div className={`${styles.cube} ${styles.h1} ${styles.w2} ${styles.l1}`}>
+          <div className={`${styles.face} ${styles.top}`}></div>
+          <div className={`${styles.face} ${styles.left}`}></div>
+          <div className={`${styles.face} ${styles.right}`}></div>
+        </div>
+
+        <div className={`${styles.cube} ${styles.h1} ${styles.w2} ${styles.l2}`}>
+          <div className={`${styles.face} ${styles.top}`}></div>
+          <div className={`${styles.face} ${styles.left}`}></div>
+          <div className={`${styles.face} ${styles.right}`}></div>
+        </div>
+
+        <div className={`${styles.cube} ${styles.h1} ${styles.w2} ${styles.l3}`}>
+          <div className={`${styles.face} ${styles.top}`}></div>
+          <div className={`${styles.face} ${styles.left}`}></div>
+          <div className={`${styles.face} ${styles.right}`}></div>
+        </div>
+
+        <div className={`${styles.cube} ${styles.h1} ${styles.w3} ${styles.l1}`}>
+          <div className={`${styles.face} ${styles.top}`}></div>
+          <div className={`${styles.face} ${styles.left}`}></div>
+          <div className={`${styles.face} ${styles.right}`}></div>
+        </div>
+
+        <div className={`${styles.cube} ${styles.h1} ${styles.w3} ${styles.l2}`}>
+          <div className={`${styles.face} ${styles.top}`}></div>
+          <div className={`${styles.face} ${styles.left}`}></div>
+          <div className={`${styles.face} ${styles.right}`}></div>
+        </div>
+
+        <div className={`${styles.cube} ${styles.h1} ${styles.w3} ${styles.l3}`}>
+          <div className={`${styles.face} ${styles.top}`}></div>
+          <div className={`${styles.face} ${styles.left}`}></div>
+          <div className={`${styles.face} ${styles.right}`}></div>
+        </div>
+      </div>
+
+      <div className={styles.h2Container}>
+        <div className={`${styles.cube} ${styles.h2} ${styles.w1} ${styles.l1}`}>
+          <div className={`${styles.face} ${styles.top}`}></div>
+          <div className={`${styles.face} ${styles.left}`}></div>
+          <div className={`${styles.face} ${styles.right}`}></div>
+        </div>
+
+        <div className={`${styles.cube} ${styles.h2} ${styles.w1} ${styles.l2}`}>
+          <div className={`${styles.face} ${styles.top}`}></div>
+          <div className={`${styles.face} ${styles.left}`}></div>
+          <div className={`${styles.face} ${styles.right}`}></div>
+        </div>
+
+        <div className={`${styles.cube} ${styles.h2} ${styles.w1} ${styles.l3}`}>
+          <div className={`${styles.face} ${styles.top}`}></div>
+          <div className={`${styles.face} ${styles.left}`}></div>
+          <div className={`${styles.face} ${styles.right}`}></div>
+        </div>
+
+        <div className={`${styles.cube} ${styles.h2} ${styles.w2} ${styles.l1}`}>
+          <div className={`${styles.face} ${styles.top}`}></div>
+          <div className={`${styles.face} ${styles.left}`}></div>
+          <div className={`${styles.face} ${styles.right}`}></div>
+        </div>
+
+        <div className={`${styles.cube} ${styles.h2} ${styles.w2} ${styles.l2}`}>
+          <div className={`${styles.face} ${styles.top}`}></div>
+          <div className={`${styles.face} ${styles.left}`}></div>
+          <div className={`${styles.face} ${styles.right}`}></div>
+        </div>
+
+        <div className={`${styles.cube} ${styles.h2} ${styles.w2} ${styles.l3}`}>
+          <div className={`${styles.face} ${styles.top}`}></div>
+          <div className={`${styles.face} ${styles.left}`}></div>
+          <div className={`${styles.face} ${styles.right}`}></div>
+        </div>
+
+        <div className={`${styles.cube} ${styles.h2} ${styles.w3} ${styles.l1}`}>
+          <div className={`${styles.face} ${styles.top}`}></div>
+          <div className={`${styles.face} ${styles.left}`}></div>
+          <div className={`${styles.face} ${styles.right}`}></div>
+        </div>
+
+        <div className={`${styles.cube} ${styles.h2} ${styles.w3} ${styles.l2}`}>
+          <div className={`${styles.face} ${styles.top}`}></div>
+          <div className={`${styles.face} ${styles.left}`}></div>
+          <div className={`${styles.face} ${styles.right}`}></div>
+        </div>
+
+        <div className={`${styles.cube} ${styles.h2} ${styles.w3} ${styles.l3}`}>
+          <div className={`${styles.face} ${styles.top}`}></div>
+          <div className={`${styles.face} ${styles.left}`}></div>
+          <div className={`${styles.face} ${styles.right}`}></div>
+        </div>
+      </div>
+
+      <div className={styles.h3Container}>
+        <div className={`${styles.cube} ${styles.h3} ${styles.w1} ${styles.l1}`}>
+          <div className={`${styles.face} ${styles.top}`}></div>
+          <div className={`${styles.face} ${styles.left}`}></div>
+          <div className={`${styles.face} ${styles.right}`}></div>
+        </div>
+
+        <div className={`${styles.cube} ${styles.h3} ${styles.w1} ${styles.l2}`}>
+          <div className={`${styles.face} ${styles.top}`}></div>
+          <div className={`${styles.face} ${styles.left}`}></div>
+          <div className={`${styles.face} ${styles.right}`}></div>
+        </div>
+
+        <div className={`${styles.cube} ${styles.h3} ${styles.w1} ${styles.l3}`}>
+          <div className={`${styles.face} ${styles.top}`}></div>
+          <div className={`${styles.face} ${styles.left}`}></div>
+          <div className={`${styles.face} ${styles.right}`}></div>
+        </div>
+
+        <div className={`${styles.cube} ${styles.h3} ${styles.w2} ${styles.l1}`}>
+          <div className={`${styles.face} ${styles.top}`}></div>
+          <div className={`${styles.face} ${styles.left}`}></div>
+          <div className={`${styles.face} ${styles.right}`}></div>
+        </div>
+
+        <div className={`${styles.cube} ${styles.h3} ${styles.w2} ${styles.l2}`}>
+          <div className={`${styles.face} ${styles.top}`}></div>
+          <div className={`${styles.face} ${styles.left}`}></div>
+          <div className={`${styles.face} ${styles.right}`}></div>
+        </div>
+
+        <div className={`${styles.cube} ${styles.h3} ${styles.w2} ${styles.l3}`}>
+          <div className={`${styles.face} ${styles.top}`}></div>
+          <div className={`${styles.face} ${styles.left}`}></div>
+          <div className={`${styles.face} ${styles.right}`}></div>
+        </div>
+
+        <div className={`${styles.cube} ${styles.h3} ${styles.w3} ${styles.l1}`}>
+          <div className={`${styles.face} ${styles.top}`}></div>
+          <div className={`${styles.face} ${styles.left}`}></div>
+          <div className={`${styles.face} ${styles.right}`}></div>
+        </div>
+
+        <div className={`${styles.cube} ${styles.h3} ${styles.w3} ${styles.l2}`}>
+          <div className={`${styles.face} ${styles.top}`}></div>
+          <div className={`${styles.face} ${styles.left}`}></div>
+          <div className={`${styles.face} ${styles.right}`}></div>
+        </div>
+
+        <div className={`${styles.cube} ${styles.h3} ${styles.w3} ${styles.l3}`}>
+          <div className={`${styles.face} ${styles.top}`}></div>
+          <div className={`${styles.face} ${styles.left}`}></div>
+          <div className={`${styles.face} ${styles.right}`}></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingAnimation;
