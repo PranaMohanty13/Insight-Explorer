b90d849749acbf9557606d96f4d66a343b814e2c
Prana Mohanty <94722016+PranaMohanty13@users.noreply.github.com>
2025-04-14 21:58:10 -0500

    "updating imports"

--- insight-explorer/lib/deepseek-client.ts
+++
@@ -1,9 +0,0 @@
-import OpenAI from "openai";
-
-
-const deepseekClient = new OpenAI({
-  baseURL: 'https://api.deepseek.com',
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
-export default deepseekClient;
